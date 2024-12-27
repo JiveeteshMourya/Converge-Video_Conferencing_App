c8e449c1b4bdd7a3d35690140b8d641378e3ed8c
Jiveetesh Mourya <jcodestudy@gmail.com>
2024-12-27 22:31:01 +0530

    authentication routes

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -17,8 +18,13 @@ app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
+app.use("/api/v1/users", userRoutes);
+// app.use("/api/v2/users", newUserRoutes);
+    // agr kabhi purane users ko alg routes pr bhejna hai, to aise version ke hisaab se bhej skte, usually done in android apps
+
 const start = async () => {
-    const connectDB = await mongoose.connect("mongodb+srv://jcodestudy:4AifrqpWEknGLdPM@converge.as5x4.mongodb.net/video_conferencing_app?retryWrites=true&w=majority&appName=Converge")
+    const connectDB = await mongoose.connect("mongodb+srv://jcodestudy:4AifrqpWEknGLdPM@converge.as5x4.mongodb.net/video_conferencing_app?retryWrites=true&w=majority&appName=Converge");
+    // console.log(connectDB.connection.name);
     console.log(`Mongo connected to host: ${connectDB.connection.host}`);
     server.listen(app.get("port"), () => {
         console.log(`Server listening on port: ${app.get("port")}`);
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
-    password: {type: String, rquired: true},
+    password: {type: String, required: true},
     token: {type: String},
 });
 
