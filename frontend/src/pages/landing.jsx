a33bfc4f2c7dd1d1e29baaece61fe18ebea4ebb4
Jiveetesh Mourya <jcodestudy@gmail.com>
2025-01-04 11:37:31 +0530

    creating landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
-export default function landing() {
+export default function LandingPage() {
     return (
-        <div>Landing Page</div>
+        <div className="landingPageContainer">
+            <nav>
+                <div className="navHeader">
+                    <h1>Converge</h1>
+                    <h5>Video Conferencing App</h5>
+                </div>
+                <div className="navlist">
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role="button">
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Converge</p>
+
+                    <div role="button">
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
